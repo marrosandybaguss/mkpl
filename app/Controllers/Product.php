b174a8140540853acd69babae601a6a502fa038c
marrosandybaguss <marrosandybaguss@gmail.com>
2021-04-15 00:21:42 +0700

    update data product

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -18,4 +18,19 @@ class Product extends BaseController
 		
 		return view('product');
 	}
+
+	public function update($id)
+	{
+		$this->productModel->save([
+			'id' => $id,
+			'prod_name' => $this->request->getVar('prod_name'),
+			'prod_type' => $this->request->getVar('prod_type'),
+			'prod_qty' => $this->request->getVar('prod_qty'),
+			'prod_notes' => $this->request->getVar('prod_notes')
+		]);
+
+		session()->setFlashdata('message', 'Data Product Has Been Updated');
+
+		return redirect()->to('/');
+	}
 }
